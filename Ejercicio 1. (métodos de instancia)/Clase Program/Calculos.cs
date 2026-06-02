ad64adda9a25fb96871be4ecde8b3bcf82bc4854
ochelott <benjash8@outlook.com>
2026-06-01 21:41:16 -0300

    Ejercicio 1. Clase Program (métodos de instancia)

---
+++ Ejercicio 1. (métodos de instancia)/Clase Program/Calculos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Program
+{
+    internal class Calculos
+    {
+        static public int Acumulado = 0;
+        static public int Cantidad = 0;
+
+        static public void RegistrarNumero(int numero)
+        {
+            Console.WriteLine("Ingrese un numero");
+            numero = Convert.ToInt32(Console.ReadLine());
+            Acumulado = Acumulado + numero;
+            Cantidad++;
+            Console.Clear();
+        }
+        static public int ObtenerAcumulado()
+        {
+            Console.Clear();
+            Console.WriteLine("El acumulado es: " + Acumulado);
+            return Acumulado;
+        }
+        static public double ObtenerPromedio()
+        {
+            return (double)Acumulado / Cantidad;
+        }
+        static public int PantallaMostrarMenu()
+        {
+            Console.WriteLine("Seleccione una opcion:");
+            Console.WriteLine("1. Registrar numero");
+            Console.WriteLine("2. Mostrar acumulado");
+            Console.WriteLine("3. Mostrar cantidad de acumulados");
+            Console.WriteLine("4. Mostrar promedio");
+            Console.WriteLine("5. Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static public void MostrarPantallaSolicitarNumero()
+        {
+            RegistrarNumero(0);
+
+        }
+        static public void MostrarPantallaAcumulado()
+        {
+            Acumulado = ObtenerAcumulado();
+        }
+        static public void MostrarPantallaCantidadIngresados()
+        {
+            Console.Clear();
+            Console.WriteLine("La cantidad de numeros acumulados es: " + Cantidad);
+        }
+        static public void MostrarPantallaPromedio()
+        {
+            Console.Clear();
+            Console.WriteLine("El promedio es: " + ObtenerPromedio());
+        }
+    }
+}
---
+++ Ejercicio 1. (métodos de instancia)/Clase Program/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do 
+            {
+                opcion = Calculos.PantallaMostrarMenu();
+                switch (opcion)
+                {
+                    case 1:
+                        Calculos.MostrarPantallaSolicitarNumero();
+                        break;
+                    case 2:
+                        Calculos.MostrarPantallaAcumulado();
+                        break;
+                    case 3:
+                        Calculos.MostrarPantallaCantidadIngresados();
+                        break;
+                    case 4:
+                        Calculos.MostrarPantallaPromedio();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida. Intente nuevamente.");
+                        break;
+                }
+            } while (opcion != 5) ;
+            Console.ReadKey();
+            Console.WriteLine("Presione cualquier tecla para salir...");
+
+        }
+    }
+}

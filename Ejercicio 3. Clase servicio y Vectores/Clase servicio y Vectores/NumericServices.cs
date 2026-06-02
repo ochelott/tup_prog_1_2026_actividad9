3c4bd30cee5c05bbef35c6c4ee8d552ae24bb6d8
ochelott <benjash8@outlook.com>
2026-06-01 22:51:40 -0300

    Ejercicio 3. Clase servicio y Vectores

---
+++ Ejercicio 3. Clase servicio y Vectores/Clase servicio y Vectores/NumericServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_servicio_y_Vectores
+{
+    internal class NumericServices
+    {
+        public int[] Numeros = new int [100];
+        public int Cantidad;
+
+        public void RegistrarNumero(int Numero)
+        {
+            Numeros[Cantidad] = Numero;
+            Cantidad++;
+        }
+        public int ObtenerAcumulado()
+        {
+            int suma = 0;
+            for (int i = 0; i < Cantidad; i++)
+            {
+                suma += Numeros[i];
+            }
+            return suma;
+        }
+        public double ObtenerPromedio()
+        {
+            double promedio = 0;
+            if (Cantidad > 0) 
+            {
+                promedio = (double) ObtenerAcumulado() / Cantidad;
+            }
+            return promedio;
+        }
+        public void ObtenerNumeros(int indiceDesde, int indiceHasta)
+        {
+            for (int i = indiceDesde; i <= indiceHasta; i++)
+            {
+                Console.WriteLine(Numeros[i]);
+            }
+        }
+    }
+}
---
+++ Ejercicio 3. Clase servicio y Vectores/Clase servicio y Vectores/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase_servicio_y_Vectores
+{
+    internal class Program
+    {
+        static NumericServices servicio = new NumericServices();
+
+        static int PantallaMostrarMenu()
+        {
+            Console.WriteLine("Seleccione una opcion:");
+            Console.WriteLine("1. Registrar numero");
+            Console.WriteLine("2. Mostrar acumulado");
+            Console.WriteLine("3. Mostrar cantidad de acumulados");
+            Console.WriteLine("4. Mostrar promedio");
+            Console.WriteLine("5. Salir");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        static void MostrarPantallaSolicitarNumero()
+        {
+
+            Console.WriteLine("Ingrese un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            servicio.RegistrarNumero(numero);
+            Console.Clear();
+        }
+        static void MostrarPantallaAcumulado()
+        {
+            Console.Clear();
+            Console.WriteLine("El acumulado es: " + servicio.ObtenerAcumulado());
+        }
+        static void MostrarPantallaCantidadIngresados()
+        {
+            Console.Clear();
+            Console.WriteLine("La cantidad de numeros acumulados es: " + servicio.Cantidad);
+        }
+        static void MostrarPantallaPromedio()
+        {
+            Console.Clear();
+            Console.WriteLine("El promedio es: " + servicio.ObtenerPromedio());
+        }
+        static void Main(string[] args)
+        {
+            int opcion;
+
+            do
+            {
+                    opcion = PantallaMostrarMenu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        MostrarPantallaSolicitarNumero();
+                        break;
+                    case 2:
+                        MostrarPantallaAcumulado();
+                        break;
+                    case 3:
+                        MostrarPantallaCantidadIngresados();
+                        break;
+                    case 4:
+                        MostrarPantallaPromedio();
+                        break;
+                    case 5:
+                        Console.WriteLine("Saliendo del programa...");
+                        break;
+                    default:
+                        Console.WriteLine("Opcion no valida, intente nuevamente.");
+                        break;
+                }
+            } while (opcion != 5);
+            
+            Console.WriteLine("Ingrese cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
